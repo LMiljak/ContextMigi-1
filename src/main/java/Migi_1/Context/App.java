aadd2c1ddf0bde7fd2dde8740e578d140d828b76
Luka Miljak <lmiljak@student.tudelft.nl>
2016-04-21 12:18:44 +0200

    Travis CI test (ignore this commit)
    Test for Travis CI

--- src/main/java/Migi_1/Context/App.java
+++ src/main/java/Migi_1/Context/App.java
@@ -7,6 +7,6 @@ package Migi_1.Context;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! " );
     }
 }

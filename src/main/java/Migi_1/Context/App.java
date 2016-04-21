a28430ae8dfecd864752f97bd7070f422814eb1f
Luka Miljak <lmiljak@student.tudelft.nl>
2016-04-21 11:30:58 +0200

    Initial Commit
    - Created the inital Maven Project
    - Turned that project into a git repository

---
+++ src/main/java/Migi_1/Context/App.java
@@ -0,0 +1,13 @@
+package Migi_1.Context;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

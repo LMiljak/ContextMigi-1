d6bfc62f4873f1350db63ba1eb988da47e91f661
Damian Voorhout <Damianvoorhout@gmail.com>
2016-05-06 17:24:40 +0200

    made some variables final, changed some lighting

--- src/main/java/com/github/migi_1/Context/design/Environment.java
+++ src/main/java/com/github/migi_1/Context/design/Environment.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.migi_1.Context.design;
 
 import com.jme3.asset.AssetManager;
@@ -23,7 +19,21 @@ import com.jme3.shadow.DirectionalLightShadowRenderer;
  * The Environement class handles all visual aspects of the world, excluding the characters and enemies etc.
  * @author Damian
  */
-public class Environment {    
+public class Environment {   
+    private static final ColorRGBA BACKGROUNDCOLOR = ColorRGBA.Blue;
+    private static final Vector3f SUNVECTOR = new Vector3f(-.5f,-.5f,-.5f);
+    private static final Vector3f SUNVECTOR_2 = new Vector3f(0, -1f, -.2f);
+    
+    private static final int SHADOWMAP_SIZE = 1024;
+    private static final int SHADOW_SPLITS = 3;
+    
+    private static final Vector3f WORLD_LOCATION = new Vector3f(0, -20, 0);
+    private static final Vector3f PLATFORM_LOCATION = new Vector3f(20, -18, -3);
+    private static final Vector3f COMMANDER_LOCATION = new Vector3f(23, -14, -3.5f);
+    private static final float COMMANDER_ROTATION = -1.5f;
+    
+    private static final float PLATFORM_SPEED = 0.02f;    
+    
     private ViewPort viewPort;
     private AssetManager assetManager;
     private Node rootNode;
@@ -32,6 +42,9 @@ public class Environment {
     private Spatial testPlatform;
     private Spatial testWorld;
     private Spatial testCommander;
+    
+    private DirectionalLight sun;
+    private DirectionalLight sun2;
     /**
      * Constructor for the environment object
      * @param flyCam, The camera for flying around in the world (will be removed)
@@ -45,65 +58,88 @@ public class Environment {
         this.assetManager = assetManager;
         this.rootNode = rootNode;
     }
-
+    
+    /**
+     * Initializes everything of the game world.
+     */
     public void init() {
         //deprecated method, it does however make it possible to load assets from a non default location
         assetManager.registerLocator("assets", FileLocator.class);           
         
         flyCam.setMoveSpeed(50);      
-        viewPort.setBackgroundColor(ColorRGBA.Blue);
+        viewPort.setBackgroundColor(BACKGROUNDCOLOR);
         
-        //creates the lights 
-        DirectionalLight sun = new DirectionalLight();
-        DirectionalLight sun2 = new DirectionalLight();
+        //creates the lights
+        initLights();        
+        
+        //creates shadowmap and attach it to the sun
+        initShadows();
+        
+        //initializes all spatials and set the positions
+        initSpatials();
+
+    }
+    
+    /**
+     * Initializes all lights of the scene.
+     */
+    private void initLights() {
+        sun = new DirectionalLight();
+        sun2 = new DirectionalLight();
 
         sun.setColor(ColorRGBA.White);
-        sun.setDirection(new Vector3f(-.5f,-.5f,-.5f).normalizeLocal());    
+        sun.setDirection(new Vector3f(SUNVECTOR).normalizeLocal());    
 
-        sun.setColor(ColorRGBA.White);
-        sun.setDirection(new Vector3f(0, -1f, -.2f).normalizeLocal());      
+        sun2.setColor(ColorRGBA.White);
+        sun2.setDirection(new Vector3f(SUNVECTOR_2).normalizeLocal());      
         
         //adds the lights to the root pane
         rootNode.addLight(sun);
         rootNode.addLight(sun2);        
-        
-        //creates shadowmap and attach it to the sun
-        final int SHADOWMAP_SIZE=1024;
-        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(assetManager, SHADOWMAP_SIZE, 3);
+    }
+    
+    /**
+     * Initializes all shadows of the scene.
+     */
+    private void initShadows() {        
+        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(assetManager, SHADOWMAP_SIZE, SHADOW_SPLITS);
         dlsr.setLight(sun);
         viewPort.addProcessor(dlsr);
         
         //adds shadow filter and attaches it to the viewport
-        DirectionalLightShadowFilter dlsf = new DirectionalLightShadowFilter(assetManager, SHADOWMAP_SIZE, 3);
+        DirectionalLightShadowFilter dlsf = new DirectionalLightShadowFilter(assetManager, SHADOWMAP_SIZE, SHADOW_SPLITS);
         dlsf.setLight(sun);
         dlsf.setEnabled(true);
         FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
         fpp.addFilter(dlsf);
         viewPort.addProcessor(fpp);
-        
-        //initializes all spatials and set the positions
+    }
+    
+    /**
+     * Initializes all objects and translations/rotations of the scene.
+     */
+    private void initSpatials() {
         testWorld = assetManager.loadModel("Models/testWorld.j3o");
-        testWorld.move(0, -20, 0);
+        testWorld.move(WORLD_LOCATION);
         testPlatform = assetManager.loadModel("Models/testPlatform.j3o");
         testPlatform.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
-        testPlatform.move(20, -18, -3);
+        testPlatform.move(PLATFORM_LOCATION);
         testCommander = assetManager.loadModel("Models/ninja.j3o");
-        testCommander.rotate(0, -1.5f, 0);
-        testCommander.move(23, -14, -3.5f);
+        testCommander.rotate(0, COMMANDER_ROTATION, 0);
+        testCommander.move(COMMANDER_LOCATION);
         
         //attach all objects to the root pane
         rootNode.attachChild(testWorld);
         rootNode.attachChild(testPlatform);
         rootNode.attachChild(testCommander);
-
     }
     
     /**
      * update the entities
      */
     public void update() {
-        testPlatform.move(-0.02f, 0, 0);
-        testCommander.move(-0.02f, 0, 0);
+        testPlatform.move(-PLATFORM_SPEED, 0, 0);
+        testCommander.move(-PLATFORM_SPEED, 0, 0);
     }
     
     /**

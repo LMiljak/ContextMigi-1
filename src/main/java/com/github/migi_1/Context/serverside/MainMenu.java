1dbafe0a5349bd78ff9e72de2209b3ca61998350
Nils <nhullegien@student.tudelft.nl>
2016-05-10 15:57:58 +0200

    Main menu works now as well

--- src/main/java/com/github/migi_1/Context/serverside/MainMenu.java
+++ src/main/java/com/github/migi_1/Context/serverside/MainMenu.java
@@ -1,15 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.migi_1.Context.serverside;
 
+import com.github.migi_1.Context.clientside.MainMenuFunctionsAndroid;
 import com.jme3.app.SimpleApplication;
 import com.jme3.niftygui.NiftyJmeDisplay;
+
 import de.lessvoid.nifty.Nifty;
-import de.lessvoid.nifty.builder.ScreenBuilder;
+import de.lessvoid.nifty.builder.ControlBuilder;
 import de.lessvoid.nifty.builder.LayerBuilder;
 import de.lessvoid.nifty.builder.PanelBuilder;
+import de.lessvoid.nifty.builder.ScreenBuilder;
 import de.lessvoid.nifty.builder.TextBuilder;
 import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
 /**
@@ -17,7 +16,8 @@ import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
  * @author Remi
  */
 public class MainMenu extends SimpleApplication {
-    
+
+    private Nifty nifty;
     /**
      * main function
      * @param args String[]
@@ -26,169 +26,194 @@ public class MainMenu extends SimpleApplication {
         MainMenu app = new MainMenu();
         app.start();
     }
-    
+
     /**
      * Builds the menu.
      */
-     @Override
-    public void simpleInitApp() {
-    NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
-            assetManager, inputManager, audioRenderer, guiViewPort);
-    Nifty nifty = niftyDisplay.getNifty();
-    guiViewPort.addProcessor(niftyDisplay);
-    flyCam.setDragToRotate(true);
- 
-    nifty.loadStyleFile("nifty-default-styles.xml");
-    nifty.loadControlFile("nifty-default-controls.xml");
- 
-    // <screen>
-    nifty.addScreen("start", new ScreenBuilder("start") {{
-        controller(new com.github.migi_1.Context.serverside.MainMenuFunctions());
-        layer(new LayerBuilder("background") {{
-            childLayoutCenter();
-            backgroundColor("#000f");
-        }});
- 
-        layer(new LayerBuilder("foreground") {{
-                childLayoutVertical();
-                backgroundColor("#0000");
- 
-            // panel added
-            panel(new PanelBuilder("panel_top") {{
-                childLayoutCenter();
-                alignCenter();
-                backgroundColor("#f008");
-                height("25%");
-                width("75%");
-                
-                // add text
-                text(new TextBuilder() {{
-                    text("Carried Away");
-                    font("Interface/Fonts/Default.fnt");
-                    height("100%");
-                    width("100%");
-                }});
-            }});
- 
-            panel(new PanelBuilder("panel_mid") {{
-                childLayoutCenter();
-                alignCenter();
-                backgroundColor("#0f08");
-                height("50%");
-                width("75%");
-                
-                // add text
-                text(new TextBuilder() {{
-                    text("Press Start to host a server"+
-                            "\nPress Quit to exit the game");
-                    font("Interface/Fonts/Default.fnt");
-                    height("100%");
-                    width("100%");
-                }});
-            }});
- 
-            panel(new PanelBuilder("panel_bottom") {{
-                childLayoutHorizontal();
-                alignCenter();
-                backgroundColor("#00f8");
-                height("25%");
-                width("75%");
- 
-                panel(new PanelBuilder("panel_bottom_left") {{
-                    childLayoutCenter();
-                    valignCenter();
-                    backgroundColor("#44f8");
-                    height("50%");
-                    width("50%");
-                    
-                    // add control
-                    control(new ButtonBuilder("StartButton", "Start") {{
-                      alignCenter();
-                      valignCenter();
-                      height("50%");
-                      width("50%");
-                      visibleToMouse(true);
-                      interactOnClick("toScreen(host)");
-                    }});
-                }});
- 
-                panel(new PanelBuilder("panel_bottom_right") {{
-                    childLayoutCenter();
-                    valignCenter();
-                    backgroundColor("#88f8");
-                    height("50%");
-                    width("50%");
-                    
-                    // add control
-                    control(new ButtonBuilder("QuitButton", "Quit") {{
-                      alignCenter();
-                      valignCenter();
-                      height("50%");
-                      width("50%");
-                      visibleToMouse(true);
-                      interactOnClick("quitGame()");
-                    }});
- 
-                }});
-            }}); // panel added
-        }});
- 
-    }}.build(nifty));
-    // </screen>
-    
-    nifty.addScreen("host", new ScreenBuilder("host"){{
-        controller(new com.github.migi_1.Context.serverside.MainMenuFunctions());
- 
-         // layer added
-         layer(new LayerBuilder("background") {{
-            childLayoutCenter();
-            backgroundColor("#000f"); 
-         }});
- 
-         layer(new LayerBuilder("foreground") {{
-                childLayoutVertical();
-                backgroundColor("#0000");        
- 
-            panel(new PanelBuilder("panel_mid") {{
-                childLayoutCenter();
-                alignCenter();
-                backgroundColor("#0f08");
-                height("50%");
-                width("75%");
-                
-                // add text
-                text(new TextBuilder() {{
-                    text("There is literally nothing here, go back");
-                    font("Interface/Fonts/Default.fnt");
-                    height("100%");
-                    width("100%");
-                }});
-            }});
- 
-            panel(new PanelBuilder("panel_bottom") {{
-                childLayoutHorizontal();
-                alignCenter();
-                backgroundColor("#00f8");
-                height("25%");
-                width("75%");
-                
-                // add control
-                control(new ButtonBuilder("BackButton", "Back") {{
-                   alignCenter();
-                   valignCenter();
-                   height("50%");
-                   width("50%");
-                   visibleToMouse(true);
-                   interactOnClick("toScreen(start)");
-                }});
+    @Override
+   public void simpleInitApp() {
+       NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+               assetManager, inputManager, audioRenderer, guiViewPort);
+       nifty = niftyDisplay.getNifty();
+       guiViewPort.addProcessor(niftyDisplay);
+       flyCam.setDragToRotate(true);
 
-            }}); // panel added
-         }});
-         // layer added
- 
-      }}.build(nifty));
- 
-    nifty.gotoScreen("start"); // start the screen
-    }
-    
+       nifty.loadStyleFile("nifty-default-styles.xml");
+       nifty.loadControlFile("nifty-default-controls.xml");
+
+       createStartScreen();
+       createJoinScreen();
+
+       nifty.gotoScreen("start"); // start the screen
+   }
+
+     private void createStartScreen() {
+         nifty.addScreen("start", new ScreenBuilder("start") {{
+             controller(new com.github.migi_1.Context.clientside.MainMenuFunctionsAndroid());
+             layer(createBGLayer());
+
+             layer(new LayerBuilder("foreground") {{
+                     childLayoutVertical();
+                     backgroundColor("#0000");
+                     panel(panelWithText("panel_top", "center", "center", "none", "#f008", "25%", "75%",
+                             createDefaultText("CarriedAway")));
+                     panel(panelWithText("panel_mid", "center", "center", "center", "#0f08", "50%", "75%",
+                             createDefaultText("Press Start to host a server \n Press Quit to exit the game")));
+
+                 panel(new PanelBuilder("panel_bottom") {{
+                     childLayoutHorizontal();
+                     alignCenter();
+                     backgroundColor("#00f8");
+                     height("25%");
+                     width("75%");
+                     panel(panelWithControl("panel_botton_left", "center", "none", "center", "#44f8", "50%", "50%",
+                             createButtonControl("StartButton", "Start", "none", "center", "center", "50%", "50%", "toScreen(join)")));
+                     panel(panelWithControl("panel_botton_right", "center", "none", "center", "#88f8", "50%", "50%",
+                             createButtonControl("QuitButton", "Quit", "none", "center", "center", "50%", "50%", "quitGame()")));
+
+                 }}); // panel added
+             }});
+
+         }}.build(nifty));
+     }
+
+     private void createJoinScreen() {
+         nifty.addScreen("join", new ScreenBuilder("join"){{
+             controller(new MainMenuFunctionsAndroid());
+             //Add bg layer
+             layer(createBGLayer());
+
+             layer(new LayerBuilder("foreground") {{
+                     childLayoutVertical();
+                     backgroundColor("#0000");
+
+                     panel(panelWithText("panel_mid", "center", "center", "none", "#0f08", "50%", "75%",
+                             createDefaultText("There is literally nothing here, go back")));
+
+                     panel(panelWithControl("panel_bottom", "horizontal", "center", "none", "#00f8", "75%", "25%",
+                             createButtonControl("BackButton", "Back", "none", "center", "center", "50%", "50%", "toScreen(start)")));
+
+             }});
+             // layer added
+
+           }}.build(nifty));
+
+     }
+
+     private LayerBuilder createBGLayer() {
+         return new LayerBuilder("background") {{
+             childLayoutCenter();
+             backgroundColor("#000f");
+          }};
+     }
+
+     private TextBuilder createDefaultText(String txt) {
+         return new TextBuilder() {{
+             text(txt);
+             font("Interface/Fonts/Default.fnt");
+             height("100%");
+             width("100%");
+         }};
+     }
+
+     private ButtonBuilder createButtonControl(String buttonId, String buttonLabel, String childLayout, String align, String valign, String height, String width, String func) {
+         return new ButtonBuilder(buttonId, buttonLabel) {{
+             switch (childLayout) {
+                 case "none" : break;
+                 case "center" :
+                     childLayoutCenter();
+                     break;
+                 default : throw new IllegalStateException("Programmer, you screwed up ");
+             }
+
+             switch (align) {
+                 case "none" : break;
+                 case "center":
+                     alignCenter();
+                     break;
+                 default : throw new IllegalStateException("Programmer, you screwed up ");
+             }
+
+             switch (valign) {
+                 case "none" : break;
+                 case "center":
+                     valignCenter();
+                     break;
+                 default: throw new IllegalStateException("Programmer, you screwed up ");
+             }
+             height(height);
+             width(width);
+             interactOnClick(func);
+          }};
+     }
+
+     private PanelBuilder panelWithText(String id, String childLayout, String align, String valign, String color, String height, String width, TextBuilder txt) {
+         return new PanelBuilder(id) {{
+             switch (childLayout) {
+             case "none" : break;
+             case "center" :
+                 childLayoutCenter();
+                 break;
+             default : throw new IllegalStateException("Programmer, you screwed up");
+         }
+
+         switch (align) {
+             case "none" : break;
+             case "center":
+                 alignCenter();
+                 break;
+             default : throw new IllegalStateException("Programmer, you screwed up");
+         }
+
+         switch (valign) {
+             case "none" : break;
+             case "center" :
+                 valignCenter();
+                 break;
+             default: throw new IllegalStateException("Programmer, you screwed up");
+         }
+             backgroundColor(color);
+             height(height);
+             width(width);
+             text(txt);
+         }};
+     }
+
+     private PanelBuilder panelWithControl(String id, String childLayout, String align, String valign, String color, String width, String heigth, ControlBuilder con) {
+         return new PanelBuilder(id) {{
+             switch (childLayout) {
+                 case "none" : break;
+                 case "center" :
+                     childLayoutCenter();
+                     break;
+                 case "horizontal" :
+                     childLayoutHorizontal();
+                     break;
+                 default : throw new IllegalStateException("Programmer, you screwed up");
+             }
+
+             switch (align) {
+                 case "none" : break;
+                 case "center":
+                     alignCenter();
+                     break;
+                 default : throw new IllegalStateException("Programmer, you screwed up");
+             }
+
+             switch (valign) {
+                 case "none" : break;
+                 case "center" :
+                     valignCenter();
+                     break;
+                 default: throw new IllegalStateException("Programmer, you screwed up");
+             }
+             backgroundColor(color);
+             height(heigth);
+             width(width);
+
+             // add control
+             control(con);
+         }};
+     }
+
 }
--- src/main/java/com/github/migi_1/Context/serverside/MainMenuFunctions.java
+++ src/main/java/com/github/migi_1/Context/serverside/MainMenuFunctions.java
@@ -7,6 +7,7 @@ package com.github.migi_1.Context.serverside;
 import com.jme3.app.Application;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
@@ -15,11 +16,11 @@ import de.lessvoid.nifty.screen.ScreenController;
  * @author Remi
  */
 public class MainMenuFunctions extends AbstractAppState implements ScreenController {
-    
+
     private Nifty nifty;
     private Screen screen;
     private Application app;
-    
+
     /**
      * This function initializes MainMenuFunctions and sets app.
      * @param stateManager AppStateManager
@@ -30,7 +31,7 @@ public class MainMenuFunctions extends AbstractAppState implements ScreenControl
         super.initialize(stateManager, app);
         this.app = app;
     }
-    
+
     /**
      * This function executes code every unit of time according to tpf to update
      * the gui.
@@ -43,7 +44,7 @@ public class MainMenuFunctions extends AbstractAppState implements ScreenControl
             // execute functions to update the lobby screen.
         }
     }
-    
+
     /**
      * Binds the nifty and the screen that is shown to the functions.
      * @param nifty Nifty
@@ -54,21 +55,23 @@ public class MainMenuFunctions extends AbstractAppState implements ScreenControl
         this.nifty = nifty;
         this.screen = screen;
     }
- 
+
     /**
      * Function needs to exist to be called when a screen is started.
      * Doesn't currently have any other functions.
      */
+    @Override
     public void onStartScreen() {
     }
- 
+
     /**
      * Function needs to exist to be called when a screen is ended.
      * Doesn't currently have any other functions.
      */
+    @Override
     public void onEndScreen() {
     }
-    
+
     /**
      * This function makes the program switch to a different screen.
      * @param scr String
@@ -76,12 +79,11 @@ public class MainMenuFunctions extends AbstractAppState implements ScreenControl
     public void toScreen(String scr) {
         nifty.gotoScreen(scr);
     }
-    
+
     /**
      * This function quits the game
      */
     public void quitGame() {
-        // app.stop();
         System.exit(0);
     }
 }

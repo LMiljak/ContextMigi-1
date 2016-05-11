41ef5d9c3f07ec811d9a9c2415dadd1f540efb95
Nils <nhullegien@student.tudelft.nl>
2016-05-11 11:59:45 +0200

    Added screens for andoid as well

---
+++ ContextApp/src/main/java/com/github/migi_1/ContextApp/Screens/MainMenuAndroidFunctions.java
@@ -0,0 +1,86 @@
+package com.github.migi_1.ContextApp.Screens;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+/**
+ * This class contains all the functions for the MainMenuAndroid screen.
+ * @author Remi & Nils
+ */
+public class MainMenuAndroidFunctions extends AbstractAppState implements ScreenController {
+
+    private Nifty nifty;
+    private Screen screen;
+    private Application app;
+
+    /**
+     * This function initializes MainMenuFunctionsAndroid and sets app.
+     * @param stateManager AppStateManager
+     * @param app Application
+     */
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = app;
+    }
+
+    /**
+     * This function executes code every unit of time according to tpf to update
+     * the gui
+     * Is currently not used.
+     * @param tpf float
+     */
+    @Override
+    public void update(float tpf) {
+    }
+
+    /**
+     * Binds the nifty and the screen that is shown to the functions.
+     * @param nifty Nifty
+     * @param screen Screen
+     */
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
+        this.screen = screen;
+    }
+
+    /**
+     * Function needs to exist to be called when a screen is started.
+     * Doesn't have any other functions currently.
+     */
+    @Override
+    public void onStartScreen() {
+    }
+
+    /**
+     * Function needs to exist to be called when a screen is ended.
+     * Doesn't currently have any other functions.
+     */
+    @Override
+    public void onEndScreen() {
+    }
+
+    /**
+     * This function makes the program switch to a different screen.
+     * @param scr (String) the id of the screen to go to.
+     */
+    public void toScreen(String scr) {
+        nifty.gotoScreen(scr);
+    }
+
+    /**
+     * This function quits the game
+     * @NOTE For now this exits the game using a System.exit(0),
+     * @NOTE in the end app.stop() will probably be a bit nicer (but it is not possible to do so yet).
+     */
+    public void quitGame() {
+        System.exit(0);
+    }
+
+}

9004b1eacb74d2d52f44b82faaf29060c63d24f2
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-11 11:24:32 +0200

    implemented listener interface

--- ContextApp/src/main/java/com/github/migi_1/ContextApp/HelloActivity.java
+++ ContextApp/src/main/java/com/github/migi_1/ContextApp/HelloActivity.java
@@ -3,6 +3,8 @@ package com.github.migi_1.ContextApp;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,7 +12,7 @@ import com.jme3.app.AndroidHarness;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 
-public class HelloActivity extends AndroidHarness {
+public class HelloActivity extends AndroidHarness implements SensorEventListener{
 
         private SensorManager mSensorManager;
         private Sensor mSensor;
@@ -32,12 +34,29 @@ public class HelloActivity extends AndroidHarness {
 //        // Set the default logging level (default=Level.INFO, Level.ALL=All Debug Info)
 //        LogManager.getLogManager().getLogger("").setLevel(Level.INFO);
     }
+
+         @Override  
+    protected void onResume()  
+    {  
+        super.onResume();
+        //mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_FASTEST);
+    }
         
         @Override  
     public void onCreate(Bundle savedInstanceState)  
     {  
         super.onCreate(savedInstanceState);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE); 
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent se) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }

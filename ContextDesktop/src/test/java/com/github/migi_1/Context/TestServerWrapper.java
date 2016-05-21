42eacbf9c28e2d75ec82cbe4de49ccfee1b7db13
Luka Miljak <lmiljak@student.tudelft.nl>
2016-05-21 21:20:58 +0200

    Test for getInstance in ServerWrapper.

--- ContextDesktop/src/test/java/com/github/migi_1/Context/TestServerWrapper.java
+++ ContextDesktop/src/test/java/com/github/migi_1/Context/TestServerWrapper.java
@@ -1,21 +1,53 @@
 package com.github.migi_1.Context;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.powermock.reflect.Whitebox;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.jme3.network.Network;
 import com.jme3.network.Server;
 
 /**
  * Test suite for the ServerWrapper class.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Network.class)
 public class TestServerWrapper {
 
+	/** The port on which the server is running. */
+	private static final int PORT = 4321;
+	
+	private Server server;
+	
+	/**
+	 * Initialises the attributes in this class used for testing.
+	 */
+	@Before
+	public void setUp() {
+		server = Mockito.mock(Server.class);
+		
+		PowerMockito.mockStatic(Network.class);
+		try {
+			Mockito.when(Network.createServer(PORT)).thenReturn(server);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Checks that the getInstance method throws an IllegalStateException if the class hasn't been initialised yet.
+	 */
+	@Test(expected = IllegalStateException.class)
+	public void testGetInstanceNotInitialised() {
+		ServerWrapper.getInstance();
+	}
+	
 }

2d945763618a6befd3af6c650d4ce9b3d96a509a
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-23 20:02:35 +0200

    added StaticMoveBehaviour

--- ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/MoveBehaviour.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/MoveBehaviour.java
@@ -3,18 +3,16 @@ package com.github.migi_1.Context.model.entity;
 import com.jme3.math.Vector3f;
 
 /**
- * class for handling the creation of a certain moving behaviour for an entity. 
+ * class for handling the creation of a certain moving behaviour for an entity.
  * @author Damian
  *
  */
-public class MoveBehaviour {
-    
+public abstract class MoveBehaviour {
+
     /**
      * Creates a certain movable behaviour for an entity.
      * @return Vector3f for the entity to move to.
      */
-    public Vector3f getMoveVector() {
-        return new Vector3f(0, 0, 0);
-    }
-    
+    public abstract Vector3f getMoveVector();
+
 }

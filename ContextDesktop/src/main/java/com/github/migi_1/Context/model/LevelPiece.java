18aa1ff77d9d661ef9836fb514e3bf12bef57ce6
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-23 11:42:33 +0200

    added LevelPiece class

---
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/LevelPiece.java
@@ -0,0 +1,62 @@
+package com.github.migi_1.Context.model;
+
+import java.util.Random;
+
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+public class LevelPiece implements IMovable {
+
+
+    private static final int DIFFERENT_WORLDS = 5;
+
+    private Spatial model;
+
+    public LevelPiece() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Models/world");
+        sb.append(new Random().nextInt(DIFFERENT_WORLDS) + 1);
+        sb.append(".j3o");
+        this.model = ProjectAssetManager.getInstance().getAssetManager().loadModel(sb.toString());
+    }
+
+    @Override
+    public Spatial getModel() {
+        return model;
+    }
+
+    @Override
+    public void setModel(Spatial model) {
+        this.model = model;
+
+    }
+
+    @Override
+    public void move(float x, float y, float z) {
+        model.move(x, y, z);
+
+    }
+
+    @Override
+    public void scale(float f) {
+        model.scale(f);
+
+    }
+
+    @Override
+    public void move(Vector3f add) {
+        model.move(add);
+
+    }
+
+    @Override
+    public MovableBehaviour getMovableBehaviour() {
+        return null;
+    }
+
+    @Override
+    public void SetMovableBehaviour(MovableBehaviour mbh) {
+
+    }
+
+}

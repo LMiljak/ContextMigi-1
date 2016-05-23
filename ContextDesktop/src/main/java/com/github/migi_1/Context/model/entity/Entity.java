b02dcb3c7161355382cd8da016662600f80d578d
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-23 21:00:45 +0200

    added comments

--- ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/Entity.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/Entity.java
@@ -1,13 +1,30 @@
 package com.github.migi_1.Context.model.entity;
 
+/**
+ * Superclass of all object that implement some MovableBehaviour.
+ *
+ * @author Marcel
+ *
+ */
 public abstract class Entity {
 
+    /**
+     * Move behaviour of particular Entity.
+     */
     protected MoveBehaviour moveBehaviour;
 
+    /**
+     * Get the MoveBehaviour of the Entity.
+     * @return MoveBehaviour
+     */
     public MoveBehaviour getMoveBehaviour() {
         return moveBehaviour;
     }
 
+    /**
+     * Set the MoveBehaviour of the Entity.
+     * @param moveBehaviour MoveBehaviour
+     */
     public void setMoveBehaviour(MoveBehaviour moveBehaviour) {
         this.moveBehaviour = moveBehaviour;
     }

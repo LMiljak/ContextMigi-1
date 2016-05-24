e3ea141b6d2f20b2f5e5509609fe191baa9ec405
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-24 11:45:45 +0200

    changed test entity class

--- ContextDesktop/src/test/java/com/git/migi_1/Context/entity/TestEntity.java
+++ ContextDesktop/src/test/java/com/git/migi_1/Context/entity/TestEntity.java
@@ -1,44 +1,24 @@
 package com.git.migi_1.Context.entity;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import com.github.migi_1.Context.model.entity.Entity;
 import com.github.migi_1.Context.model.entity.MoveBehaviour;
-import com.jme3.math.Vector3f;
 
 
 @PrepareForTest({Entity.class})
-public class TestEntity {
+public abstract class TestEntity {
 
     private Entity testEntity;
 
     private MoveBehaviour testMoveBehaviour;
 
     @Before
-    public void setUp() {
-        testEntity = Mockito.spy(Entity.class);
-        testMoveBehaviour = Mockito.spy(MoveBehaviour.class);
-    }
+    public abstract void setUp();
 
-    @Test
-    public void testGetMoveBehaviour() {
-        assertEquals(testEntity.getMoveBehaviour().getMoveVector(), new Vector3f(0, 0, 0));
+    protected void setMoveBehaviour(MoveBehaviour moveBehaviour) {
+        testMoveBehaviour = moveBehaviour;
     }
-
-    @Test
-    public void testSetMoveBehaviour() {
-        testEntity.setMoveBehaviour(testMoveBehaviour);
-        assertEquals(testEntity.getMoveBehaviour(), testMoveBehaviour);
-    }
-//
-//    @Test
-//    public void testGetModel() {
-//        assertEquals(testEntity.getModel(), null);
-//    }
 
 }

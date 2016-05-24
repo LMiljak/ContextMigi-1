6a1a73ed5b1a0d93e61e97b6252350f2dc312bca
Nils <nhullegien@student.tudelft.nl>
2016-05-24 12:01:07 +0200

    Tested ProjectAssetManager class

---
+++ ContextDesktop/src/test/java/com/github/migi_1/Context/utility/TestProjectAssetManager.java
@@ -0,0 +1,42 @@
+package com.github.migi_1.Context.utility;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.jme3.asset.AssetManager;
+
+public class TestProjectAssetManager {
+
+    private AssetManager assetManager;
+    private ProjectAssetManager projAssetManager;
+
+    @Before
+    public void setup() {
+        assetManager = Mockito.mock(AssetManager.class);
+        projAssetManager = ProjectAssetManager.getInstance();
+    }
+
+    /**
+     * Verify the instance returned by the getInstance method is a projectAssetManager.
+     */
+    @Test
+    public void getInstanceTest() {
+        assertEquals(projAssetManager.getClass(), ProjectAssetManager.class);
+    }
+
+    /**
+     * Verify the get and set AssetManager methods work correctly.
+     */
+    @Test
+    public void getAndSetAssetManager() {
+        //Verify there is no assetManager set.
+        assertNotEquals(projAssetManager.getAssetManager(), assetManager);
+        projAssetManager.setAssetManager(assetManager);
+        assertEquals(projAssetManager.getAssetManager(), assetManager);
+    }
+
+}

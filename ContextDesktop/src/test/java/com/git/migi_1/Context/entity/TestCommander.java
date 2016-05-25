29344e8ca8605287498875353612fb25338dd32f
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-25 17:23:36 +0200

    added testCommander class

---
+++ ContextDesktop/src/test/java/com/git/migi_1/Context/entity/TestCommander.java
@@ -0,0 +1,72 @@
+package com.git.migi_1.Context.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.github.migi_1.Context.Main;
+import com.github.migi_1.Context.model.entity.Commander;
+import com.github.migi_1.Context.model.entity.MoveBehaviour;
+import com.github.migi_1.Context.utility.ProjectAssetManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.collision.CollisionResults;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ProjectAssetManager.class, AssetManager.class})
+public class TestCommander extends TestEntity {
+
+    Commander testCommander;
+
+    ProjectAssetManager pAssetManager;
+
+    AssetManager assetManager;
+
+    MoveBehaviour moveBehaviour;
+
+    Main main;
+
+    Spatial model;
+
+    Vector3f vector;
+
+    Integer id;
+
+
+    @Override
+    @Before
+    public void setUp() {
+
+        pAssetManager = PowerMockito.mock(ProjectAssetManager.class);
+        assetManager = Mockito.mock(AssetManager.class);
+        model =  Mockito.mock(Spatial.class);
+        moveBehaviour = Mockito.mock(MoveBehaviour.class);
+        PowerMockito.mockStatic(ProjectAssetManager.class);
+        BDDMockito.given(ProjectAssetManager.getInstance()).willReturn(pAssetManager);
+        BDDMockito.given(pAssetManager.getAssetManager()).willReturn(assetManager);
+        Mockito.when(assetManager.loadModel(Mockito.anyString())).thenReturn(model);
+
+        testCommander = new Commander(new Vector3f(0, 0, 0));
+
+        setModel(model);
+        setMoveBehaviour(moveBehaviour);
+        setEntity(testCommander);
+
+    }
+
+    @Test
+    public void collideWithTest() {
+        Spatial collider = Mockito.mock(Spatial.class);
+        CollisionResults results = Mockito.mock(CollisionResults.class);
+        testCommander.collideWith(collider, results);
+        Mockito.verify(model, Mockito.times(1)).collideWith(collider, results);
+    }
+
+}

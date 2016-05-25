dc9c160aff9e54a8efcf32982a28e0601c432ded
Luka Miljak <lmiljak@student.tudelft.nl>
2016-05-25 21:44:50 +0200

    Made takeDamage a default method in IKillable.

--- ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/IKillable.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/IKillable.java
@@ -22,13 +22,20 @@ public interface IKillable {
 
     /**
      * Reduces the entity's health by the given damage.
+     * Calls the onKilled method if the health reaches 0 or less
+     * after this call.
      * @param damage how much damage to deal to the entity
-     *
      */
-    void takeDamage(int damage);
+    default void takeDamage(int damage) {
+    	setHealth(getHealth() - damage);
+    	if (getHealth() <= 0) {
+    		onKilled();
+    	}
+    }
 
     /**
-     * This method is called when the object has zero health points or less.
+     * This method is called when the object takes damage and as
+     * a result of that gets 0 health or less.
      */
     void onKilled();
 

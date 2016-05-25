70792b08e46378d6ff13a489cb50517b85bc188e
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-25 17:29:05 +0200

    AccelerometerTest

---
+++ ContextDesktop/src/test/java/com/git/migi_1/Context/entity/AcceleratorMoveBehaviourTest.java
@@ -0,0 +1,26 @@
+package com.git.migi_1.Context.entity;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.migi_1.Context.model.entity.AcceleratorMoveBehaviour;
+import com.jme3.math.Vector3f;
+
+public class AcceleratorMoveBehaviourTest {
+
+    private AcceleratorMoveBehaviour testMoveBehaviour;
+
+    @Before
+    public void setUp() {
+        testMoveBehaviour = new AcceleratorMoveBehaviour();
+
+    }
+
+    @Test
+    public void testGetMoveBehaviour() {
+        assertEquals(testMoveBehaviour.getMoveVector(), new Vector3f(-0.2f, 0, 0));
+    }
+
+}

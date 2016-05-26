413626058afe957fc3c6dd5a7124176a352b1b13
Nils <nhullegien@student.tudelft.nl>
2016-05-26 23:27:17 +0200

    Added InputHandler class

--- ContextDesktop/src/main/java/com/github/migi_1/Context/InputHandler.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/InputHandler.java
@@ -1,5 +1,141 @@
 package com.github.migi_1.Context;
 
-public class InputHandler {
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.math.Vector3f;
 
+import jmevr.app.VRApplication;
+
+public final class InputHandler {
+
+    private String[] actions = {"exit", "cam_switch", "forwards", "backwards", "left", "right", "up", "down", "steer_left", "steer_right"};
+    private int[] keyInputs = {KeyInput.KEY_ESCAPE, KeyInput.KEY_C, KeyInput.KEY_W, KeyInput.KEY_S, KeyInput.KEY_A, KeyInput.KEY_D,
+                               KeyInput.KEY_SPACE, KeyInput.KEY_LSHIFT, KeyInput.KEY_LEFT, KeyInput.KEY_RIGHT};
+    private boolean forwards, back, left, right, up, down = false;
+    private Main main;
+
+    public InputHandler(Main main) {
+        this.main = main;
+    }
+
+    /**
+     * Initialises the game inputs (keys).
+     *
+     * Key bindings:
+     * Escape key: Exit the game
+     * c: switches camera
+     * When in flycam:
+     * w: move forwards
+     * s: move backwars
+     * a: move left
+     * d: move right
+     * Lshift: move down
+     * space: move up
+     * ---MORE CAN BE ADDED IF NEEDED---
+     */
+    public void initInputs(Main main) {
+        InputManager inputManager = main.getInputManager();
+        addMappings(inputManager);
+        ActionListener acl = new ActionListener() {
+
+            @Override
+            public void onAction(String name, boolean keyPressed, float tpf) {
+                if (name.equals("exit") && keyPressed) {
+                    System.exit(0);
+                } else if (name.equals("cam_switch") && keyPressed) {
+                    main.getEnv().swapCamera();
+                }
+
+                //Controls that only work with flycam.
+                if (main.getEnv().getFlyCamActive()) {
+                    switch (name) {
+                        case "forwards":
+                            forwards = keyPressed;
+                            break;
+                        case "backwards":
+                            back = keyPressed;
+                            break;
+                        case "left":
+                            left = keyPressed;
+                            break;
+                        case "right":
+                            right = keyPressed;
+                            break;
+                        case "up":
+                            up = keyPressed;
+                            break;
+                        case "down":
+                            down = keyPressed;
+                            break;
+                        default: //Do nothing when an unknown button is pressed.
+                    }
+                }
+                checkSteering(name, keyPressed);
+
+            }
+
+        };
+        addListeners(inputManager, acl);
+    }
+
+    /**
+     * Adds all the mappings for the different function names to the different keys.
+     * @param inputManager the inputmanager for which these keymappings are set.
+     */
+    private void addMappings(InputManager inputManager) {
+        for(int i = 0; i < actions.length; i++) {
+            inputManager.addMapping(actions[i], new KeyTrigger(keyInputs[i]));
+        }
+    }
+
+    private void addListeners(InputManager inputManager, ActionListener acl) {
+        for(String action : actions) {
+            inputManager.addListener(acl, action);
+        }
+    }
+
+    /**
+     * Key binding for steering.
+     * left: move left
+     * right: move right
+     * @param name Name of key action.
+     * @param keyPressed True when pressed, false when released.
+     */
+    private void checkSteering(String name, boolean keyPressed) {
+        if (keyPressed) {
+            if (name.equals("steer_left")) {
+                main.getEnv().steer(-1.f);
+            }
+            else if (name.equals("steer_right")) {
+               main.getEnv().steer(1.f);
+           }
+       }
+       if (!keyPressed && (name.equals("steer_left") || name.equals("steer_right"))) {
+           main.getEnv().steer(0.f);
+       }
+    }
+
+    public void moveCamera(float tpf) {
+        if (forwards) {
+            System.out.println(VRApplication.getFinalObserverRotation().getRotationColumn(2).mult(tpf * 8f));
+            main.getEnv().moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(2).mult(tpf * 8f));
+        }
+        if (back) {
+            main.getEnv().moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(2).mult(-tpf * 8f));
+        }
+        if (left) {
+            main.getEnv().rotateCam(new Vector3f(0f, 0.75f * tpf, 0f));
+        }
+        if (right) {
+            main.getEnv().rotateCam(new Vector3f(0, -0.75f * tpf, 0));
+        }
+        if (up) {
+            main.getEnv().moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(1).mult(tpf * 8f));
+        }
+        if (down) {
+            main.getEnv().moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(1).mult(-tpf * 8f));
+        }
+    }
 }
--- ContextDesktop/src/main/java/com/github/migi_1/Context/Main.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/Main.java
@@ -6,11 +6,6 @@ import java.util.concurrent.Executors;
 import com.github.migi_1.Context.model.MainEnvironment;
 import com.github.migi_1.Context.screens.MainMenu;
 import com.github.migi_1.Context.utility.ProjectAssetManager;
-import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.KeyTrigger;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Node;
 import com.jme3.system.AppSettings;
@@ -33,10 +28,7 @@ public class Main extends VRApplication {
     //the main application
     private static Main main;
 
-    /**
-     * Movements of the flycam.
-     */
-    private boolean forwards, back, left, right, up, down;
+    private InputHandler inputHandler;
 
     /**
      * main function of the appication, sets some meta-parameters of the application
@@ -64,7 +56,8 @@ public class Main extends VRApplication {
      */
     @Override
     public void simpleInitApp() {
-        initInputs();
+        inputHandler = new InputHandler(main);
+        inputHandler.initInputs(main);
 
         mainMenuState = new MainMenu();
         environmentState = new MainEnvironment();
@@ -97,24 +90,7 @@ public class Main extends VRApplication {
         if (getStateManager().hasState(environmentState)) {
             getStateManager().getState(MainEnvironment.class).update(tpf);
         }
-        if (forwards) {
-        	environmentState.moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(2).mult(tpf * 8f));
-        }
-        if (back) {
-        	environmentState.moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(2).mult(-tpf * 8f));
-        }
-        if (left) {
-        	environmentState.rotateCam(new Vector3f(0f, 0.75f * tpf, 0f));
-        }
-        if (right) {
-        	environmentState.rotateCam(new Vector3f(0, -0.75f * tpf, 0));
-        }
-        if (up) {
-        	environmentState.moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(1).mult(tpf * 8f));
-        }
-        if (down) {
-        	environmentState.moveCam(VRApplication.getFinalObserverRotation().getRotationColumn(1).mult(-tpf * 8f));
-        }
+        inputHandler.moveCamera(tpf);
     }
 
     /**
@@ -123,124 +99,6 @@ public class Main extends VRApplication {
      */
     @Override
     public void simpleRender(RenderManager rm) { }
-
-    /**
-     * Initialises the game inputs (keys).
-     *
-     * Key bindings:
-     * Escape key: Exit the game
-     * c: switches camera
-     * When in flycam:
-     * w: move forwards
-     * s: move backwars
-     * a: move left
-     * d: move right
-     * Lshift: move down
-     * space: move up
-     * ---MORE CAN BE ADDED IF NEEDED---
-     */
-     private void initInputs() {
-         InputManager inputManager = getInputManager();
-         addMappings(inputManager);
-         ActionListener acl = new ActionListener() {
-
-             @Override
-             public void onAction(String name, boolean keyPressed, float tpf) {
-//                 System.out.println(environmentState.getCamera().toString());
-                 if (name.equals("exit") && keyPressed) {
-                     System.exit(0);
-                 } else if (name.equals("cam_switch") && keyPressed) {
-                     environmentState.swapCamera();
-                 }
-
-                 //Controls that only work with flycam.
-                 if (environmentState.getFlyCamActive()) {
-                     switch (name) {
-                         case "forward":
-                             forwards = keyPressed;
-                             break;
-                         case "back":
-                             back = keyPressed;
-                             break;
-                         case "left":
-                             left = keyPressed;
-                             break;
-                         case "right":
-                             right = keyPressed;
-                             break;
-                         case "up":
-                             up = keyPressed;
-                             break;
-                         case "down":
-                             down = keyPressed;
-                             break;
-                         default: //Do nothing when an unknown button is pressed.
-                     }
-                 }
-                 checkSteering(name, keyPressed);
-
-             }
-
-         };
-         addListeners(inputManager, acl);
-    }
-
-     /**
-      * Key binding for steering.
-      * left: move left
-      * right: move right
-      * @param name Name of key action.
-      * @param keyPressed True when pressed, false when released.
-      */
-     private void checkSteering(String name, boolean keyPressed) {
-         if (keyPressed) {
-             if (name.equals("steer_left")) {
-                 environmentState.steer(-1.f);
-             }
-             else if (name.equals("steer_right")) {
-                environmentState.steer(1.f);
-            }
-        }
-        if (!keyPressed && (name.equals("steer_left") || name.equals("steer_right"))) {
-            environmentState.steer(0.f);
-        }
-     }
-
-    /**
-     * Adds all the mappings for the different function names to the different keys.
-     * @param inputManager the inputmanager for which these keymappings are set.
-     */
-    private void addMappings(InputManager inputManager) {
-        inputManager.addMapping("exit", new KeyTrigger(KeyInput.KEY_ESCAPE));
-        inputManager.addMapping("cam_switch", new KeyTrigger(KeyInput.KEY_C));
-        inputManager.addMapping("forward", new KeyTrigger(KeyInput.KEY_W));
-        inputManager.addMapping("back", new KeyTrigger(KeyInput.KEY_S));
-        inputManager.addMapping("left", new KeyTrigger(KeyInput.KEY_A));
-        inputManager.addMapping("right", new KeyTrigger(KeyInput.KEY_D));
-        inputManager.addMapping("up", new KeyTrigger(KeyInput.KEY_SPACE));
-        inputManager.addMapping("down", new KeyTrigger(KeyInput.KEY_LSHIFT));
-        inputManager.addMapping("steer_left", new KeyTrigger(KeyInput.KEY_LEFT));
-        inputManager.addMapping("steer_right", new KeyTrigger(KeyInput.KEY_RIGHT));
-    }
-
-    /**
-     * Adds key-input event listeners to the inputmanager.
-     * @param inputManager The input manager to which these listeners are added.
-     * @param acl The actionlistener that listens to the key-input events.
-     */
-    private void addListeners(InputManager inputManager, ActionListener acl) {
-        inputManager.addListener(acl, "exit");
-        inputManager.addListener(acl, "cam_switch");
-        inputManager.addListener(acl, "forward");
-        inputManager.addListener(acl, "back");
-        inputManager.addListener(acl, "left");
-        inputManager.addListener(acl, "right");
-        inputManager.addListener(acl, "up");
-        inputManager.addListener(acl, "down");
-        inputManager.addListener(acl, "steer_left");
-        inputManager.addListener(acl, "steer_right");
-    }
-
 
     /**
      * Steers the platform depending on the orientation of an accelerometer.

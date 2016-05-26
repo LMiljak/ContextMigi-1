55d98551f4156342360a900d61701178dbe814e5
Luka Miljak <lmiljak@student.tudelft.nl>
2016-05-26 14:11:46 +0200

    Added Javadoc

--- ContextDesktop/src/main/java/com/github/migi_1/Context/model/Environment.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/Environment.java
@@ -12,6 +12,10 @@ import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.scene.Node;
 
+/**
+ * An Environment represents a world that can contain all kinds
+ * of objects that can be added/removed.
+ */
 public class Environment extends AbstractAppState {
 
 	private Node rootNode;
@@ -32,15 +36,41 @@ public class Environment extends AbstractAppState {
 		moveMovables();
 	}
 	
+	/**
+	 * Adds a Displayable object to the world.
+	 * Note: Do not add Entities using this method.
+	 * 
+	 * @param displayable
+	 * 		The displayable to add.
+	 */
 	public void addDisplayable(IDisplayable displayable) {
 		rootNode.attachChild(displayable.getModel());
 	}
 	
+	/**
+	 * Removes a Displayable object from the world.
+	 * 
+	 * @param displayable
+	 * 		The displayable to remove.
+	 */
+	public void removeDisplayable(IDisplayable displayable) {
+		rootNode.detachChild(displayable.getModel());
+	}
+	
+	/**
+	 * Adds an Entity to the world.
+	 * 
+	 * @param entity
+	 * 		The entity to add.
+	 */
 	public void addEntity(Entity entity) {
 		addDisplayable(entity);
 		movables.add(entity);
 	}
 	
+	/**
+	 * Moves all Movable objects in the world using the MoveBehaviours.
+	 */
 	private void moveMovables() {
 		for (IMovable movable : movables) {
 			movable.move(movable.getMoveBehaviour().getMoveVector());

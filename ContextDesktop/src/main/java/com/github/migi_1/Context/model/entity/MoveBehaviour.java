a4b416f3f4fcb77a621c0aea23817b01a84b6328
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-29 18:27:59 +0200

    changed moveBehaviour superclass

--- ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/MoveBehaviour.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/MoveBehaviour.java
@@ -9,6 +9,15 @@ import com.jme3.math.Vector3f;
  */
 public abstract class MoveBehaviour {
 
+    private Vector3f moveVector;
+
+    /**
+     * Constructor for abstract MoveBehaviour where the default moveVector is static.
+     */
+    public MoveBehaviour() {
+        moveVector = new Vector3f(0, 0, 0);
+    }
+
     /**
      * Gets the move vector of this behaviour.
      * This method is called every tick of the game and this move vector will be added
@@ -17,7 +26,9 @@ public abstract class MoveBehaviour {
      * @return Vector3f
      * 		The move vector of the behaviour.
      */
-    public abstract Vector3f getMoveVector();
+    public Vector3f getMoveVector() {
+        return moveVector;
+    }
 
     /**
      * Update the moveVector.
@@ -25,6 +36,14 @@ public abstract class MoveBehaviour {
     public void updateMoveVector() {
     }
 
-    public abstract void setMoveVector(Vector3f moveVector);
+    /**
+     * Setter for the MoveVector attribute.
+     * @param moveVector attribute
+     */
+    public void setMoveVector(Vector3f moveVector) {
+        this.moveVector = moveVector;
+    }
+
+
 
 }
--- ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/StaticMoveBehaviour.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/entity/StaticMoveBehaviour.java
@@ -1,6 +1,5 @@
 package com.github.migi_1.Context.model.entity;
 
-import com.jme3.math.Vector3f;
 
 /**
  * Move behaviour of a static (non-moving) object.
@@ -10,9 +9,5 @@ import com.jme3.math.Vector3f;
  */
 public class StaticMoveBehaviour extends MoveBehaviour {
 
-    @Override
-    public Vector3f getMoveVector() {
-        return new Vector3f(0, 0, 0);
-    }
 
 }

26173c745ca70ed81fd381f861a949271460e7ef
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-30 15:18:59 +0200

    made HUBString more static

--- ContextDesktop/src/main/java/com/github/migi_1/Context/main/HUDController.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/main/HUDController.java
@@ -15,13 +15,16 @@ public class HUDController {
 
     private BitmapText hudText;
 
+    private int threshold;
+
     public HUDController(Node guiNode, Application app) {
+        threshold = 10;
         this.app = app;
         BitmapFont guiFont = ProjectAssetManager.getInstance().getAssetManager().loadFont("Interface/Fonts/RockwellExtraBold.fnt");
         hudText = new BitmapText(guiFont, false);
-        hudText.setSize(guiFont.getCharSet().getRenderedSize() * 5);      // font size
-        hudText.setColor(ColorRGBA.Red);                             // font color
-        hudText.setText("You can write any string here");             // the text
+        hudText.setSize(guiFont.getCharSet().getRenderedSize() * 4);      // font size
+        hudText.setColor(ColorRGBA.White);                             // font color
+        hudText.setText("0");             // the text
 
         hudText.setLocalTranslation(((Main) app).getSettings().getWidth() - hudText.getLineWidth(), ((Main) app).getSettings().getHeight() - hudText.getLineHeight(), 0); // position
         guiNode.attachChild(hudText);
@@ -31,7 +34,10 @@ public class HUDController {
     public void updateHUD() {
         count = count + 0.1f;
         hudText.setText(Integer.toString(Math.round(count)));
-        hudText.setLocalTranslation(((Main) app).getSettings().getWidth() - hudText.getLineWidth(), ((Main) app).getSettings().getHeight(), 0); // position
+        if (Math.round(count) >= threshold) {
+            threshold *= 10;
+            hudText.setLocalTranslation(((Main) app).getSettings().getWidth() - hudText.getLineWidth(), ((Main) app).getSettings().getHeight(), 0); // position
+        }
 
 
     }

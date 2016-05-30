340ccca6933ef3ddc7cb6ce6433c8bbbbf0d5039
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-30 17:06:28 +0200

    text in correct place

--- ContextDesktop/src/main/java/com/github/migi_1/Context/main/HUDController.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/main/HUDController.java
@@ -6,6 +6,7 @@ import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Node;
+import com.jme3.system.AppSettings;
 
 public class HUDController {
 
@@ -17,18 +18,30 @@ public class HUDController {
 
     private int threshold;
 
+    private AppSettings settings;
+
     public HUDController(Node guiNode, Application app) {
         threshold = 10;
         this.app = app;
         BitmapFont guiFont = ProjectAssetManager.getInstance().getAssetManager().loadFont("Interface/Fonts/RockwellExtraBold.fnt");
         hudText = new BitmapText(guiFont, false);
         hudText.setSize(guiFont.getCharSet().getRenderedSize() * 4);      // font size
-        hudText.setColor(ColorRGBA.White);                             // font color
+        hudText.setColor(ColorRGBA.White);
         hudText.setText("0");             // the text
+        settings = ((Main) app).getSettings();
+        hudText.setLocalTranslation(settings.getWidth() - hudText.getLineWidth(), settings.getHeight(), 0); // position
 
-        hudText.setLocalTranslation(((Main) app).getSettings().getWidth() - hudText.getLineWidth(), ((Main) app).getSettings().getHeight() - hudText.getLineHeight(), 0); // position
+
+//        Material mat = new Material(ProjectAssetManager.getInstance().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+//        mat.setColor("Color", new ColorRGBA(1, 0, 0, 0.5f)); // 0.5f is the alpha value
+
+
+        // Create rectangle of size 10x10
+//        Geometry rect = new Geometry("Rect", new Quad(hudText.getLineWidth(), hudText.getLineHeight()));
+//        rect.setLocalTranslation(settings.getWidth() - hudText.getLineWidth(), settings.getHeight() - hudText.getLineHeight(), 0);
+//        rect.setMaterial(mat);
+//        guiNode.attachChild(rect);
         guiNode.attachChild(hudText);
-
     }
 
     public void updateHUD() {
@@ -36,7 +49,7 @@ public class HUDController {
         hudText.setText(Integer.toString(Math.round(count)));
         if (Math.round(count) >= threshold) {
             threshold *= 10;
-            hudText.setLocalTranslation(((Main) app).getSettings().getWidth() - hudText.getLineWidth(), ((Main) app).getSettings().getHeight(), 0); // position
+            hudText.setLocalTranslation(settings.getWidth() - hudText.getLineWidth(), settings.getHeight(), 0); // position
         }
 
 

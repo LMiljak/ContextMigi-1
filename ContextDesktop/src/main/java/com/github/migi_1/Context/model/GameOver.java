5e7b24fcc3baca97462fc26e05243a5fdc8efaea
Marcel Kuipers <mekuipers@tudelft.nl>
2016-05-31 16:02:31 +0200

    added pausing functionality

---
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/GameOver.java
@@ -0,0 +1,6 @@
+package com.github.migi_1.Context.model;
+
+public class GameOver extends Environment {
+
+
+}
---
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/PauseState.java
@@ -0,0 +1,14 @@
+package com.github.migi_1.Context.model;
+
+public class PauseState extends MainEnvironment {
+
+    @Override
+    public void update(float tpf) {
+        super.update(0);
+
+//        checkCollision();
+//        updateTestWorld();
+    }
+
+
+}

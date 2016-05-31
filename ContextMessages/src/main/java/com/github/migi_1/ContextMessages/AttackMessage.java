fe3e1c8e7f9e75dbf29baa0805c48a49fcc56440
ThatFlint <remiflint@live.nl>
2016-05-31 15:15:18 +0200

    minor adjustment: == to equals()

--- ContextMessages/src/main/java/com/github/migi_1/ContextMessages/AttackMessage.java
+++ ContextMessages/src/main/java/com/github/migi_1/ContextMessages/AttackMessage.java
@@ -28,7 +28,7 @@ public class AttackMessage extends AbstractMessage {
      */
     public AttackMessage(PlatformPosition position, String direction) {
         this.position = position;
-        if(direction == "middle" || direction == "right") {
+        if(direction.equals("middle") || direction.equals("right")) {
             this.direction = direction;
         }
         else {

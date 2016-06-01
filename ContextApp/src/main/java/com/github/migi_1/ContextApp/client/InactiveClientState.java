2285f56a771d0f6dd72fed07bcc01f5fa68d2c9b
Luka Miljak <lmiljak@student.tudelft.nl>
2016-06-01 12:17:55 +0200

    Added Javadoc in InactiveClientState

--- ContextApp/src/main/java/com/github/migi_1/ContextApp/client/InactiveClientState.java
+++ ContextApp/src/main/java/com/github/migi_1/ContextApp/client/InactiveClientState.java
@@ -2,12 +2,25 @@ package com.github.migi_1.ContextApp.client;
 
 import com.jme3.network.Client;
 
+/**
+ * A Client state for when the Client isn't active.
+ */
 public class InactiveClientState extends ClientState {
 
+    /**
+     * Constructor for InactiveClientState.
+     * 
+     * @param client
+     *      The client that this state represents.
+     */
     public InactiveClientState(Client client) {
         super(client);
     }
     
+    /**
+     * Called when this state gets activated.
+     * The InactiveClientState closes the client.
+     */
     @Override
     public void onActivate() {
         getClient().close();

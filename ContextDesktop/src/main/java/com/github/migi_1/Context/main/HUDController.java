1551d034f70b77aa8d0bc68cec15edf791978518
Marcel Kuipers <mekuipers@tudelft.nl>
2016-06-01 16:51:31 +0200

    added basic game over screen

--- ContextDesktop/src/main/java/com/github/migi_1/Context/main/HUDController.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/main/HUDController.java
@@ -23,6 +23,8 @@ public class HUDController {
 
     private BitmapText hudText;
 
+    private BitmapFont guiFont;
+
     private int threshold = 10;
 
     private AppSettings settings;
@@ -31,6 +33,10 @@ public class HUDController {
 
     private Geometry gameOver;
 
+    private BitmapText gameOverText;
+
+    private BitmapText gameOverScore;
+
     /**
      * Constructor. Generates initial score and displays it.
      * @param app Application
@@ -38,7 +44,7 @@ public class HUDController {
     public HUDController(Application app) {
         this.app = app;
         AssetManager assetManager = ProjectAssetManager.getInstance().getAssetManager();
-        BitmapFont guiFont = assetManager.loadFont("Interface/Fonts/RockwellExtraBold.fnt");
+        guiFont = assetManager.loadFont("Interface/Fonts/RockwellExtraBold.fnt");
         hudText = new BitmapText(guiFont, false);
         hudText.setSize(guiFont.getCharSet().getRenderedSize() * 4);
         hudText.setColor(ColorRGBA.White);
@@ -53,7 +59,7 @@ public class HUDController {
 
     private void createGameOverScreen() {
         Material mat = new Material(ProjectAssetManager.getInstance().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", new ColorRGBA(1, 0, 0, 0.5f));
+        mat.setColor("Color", new ColorRGBA(0, 1, 1, 0.5f));
         float width = (float) (settings.getWidth() / 1.5);
         float height = (float) (settings.getHeight() / 1.5);
 
@@ -62,6 +68,19 @@ public class HUDController {
         float upperBound = (settings.getHeight() - height)/ 2;
         gameOver.setLocalTranslation(leftBound, upperBound, 0);
         gameOver.setMaterial(mat);
+
+        gameOverText =  new BitmapText(guiFont, false);
+        gameOverText.setSize(guiFont.getCharSet().getRenderedSize() * 4);
+        gameOverText.setColor(ColorRGBA.White);
+        gameOverText.setText("Game Over!");
+        gameOverText.setLocalTranslation((settings.getWidth() - gameOverText.getLineWidth()) / 2, settings.getHeight() - upperBound, 1);
+
+        gameOverScore =  new BitmapText(guiFont, false);
+        gameOverScore.setSize(guiFont.getCharSet().getRenderedSize() * 4);
+        gameOverScore.setColor(ColorRGBA.White);
+        gameOverScore.setLocalTranslation(settings.getWidth() / 2, height, 1);
+
+
     }
 
     /**
@@ -87,7 +106,11 @@ public class HUDController {
     }
 
     public void gameOver() {
+        gameOverScore.setText(Integer.toString(Math.round(gameScore)));
         ((Main) app).getGuiNode().attachChild(gameOver);
+        ((Main) app).getGuiNode().attachChild(gameOverText);
+        ((Main) app).getGuiNode().attachChild(gameOverScore);
+
     }
 
 }

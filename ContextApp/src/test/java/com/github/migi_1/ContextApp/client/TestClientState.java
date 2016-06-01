8cc86f4f46b99c169c0e1a007150171d0536d058
Luka Miljak <lmiljak@student.tudelft.nl>
2016-06-01 12:23:46 +0200

    Added Javadoc in TestClientState

--- ContextApp/src/test/java/com/github/migi_1/ContextApp/client/TestClientState.java
+++ ContextApp/src/test/java/com/github/migi_1/ContextApp/client/TestClientState.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.migi_1.ContextApp.client;
 
 import static org.junit.Assert.assertEquals;
@@ -10,31 +6,61 @@ import com.jme3.network.Client;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Abstract test suite for the ClienState class.
+ */
 public abstract class TestClientState {
     
     private ClientState clientState;
     private Client client;
     
+    /**
+     * Initialises the test objects. For subclasses that implement this, use
+     * the setClientState method.
+     */
     @Before
     public abstract void setUp();
     
+    /**
+     * Sets the test objects for this test suite.
+     * 
+     * @param clientState
+     *      The clientState.
+     * @param client 
+     *      The client used to create the clientState.
+     */
     protected void setClientState(ClientState clientState, Client client) {
         this.clientState = clientState;
         this.client = client;
     }
     
+    /**
+     * Gets the ClientState test object.
+     * 
+     * @return
+     *      The ClientState test object.
+     */
     protected ClientState getClientState() {
         return clientState;
     }
     
+    /**
+     * Tests the getClient method.
+     */
     @Test
     public void testGetClient() {
         assertEquals(client, clientState.getClient());
     }
     
+    /**
+     * Tests the onActivate method.
+     */
     @Test
     public abstract void testOnActivate();
     
+    /**
+     * Tests the onDeactivate method.
+     */
     @Test
     public abstract void testOnDeactivate();
 }

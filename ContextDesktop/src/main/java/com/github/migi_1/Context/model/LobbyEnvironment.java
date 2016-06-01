1926e3f7f572def3bc984f7a6d4e6edc045110d6
ThatFlint <remiflint@live.nl>
2016-06-01 13:55:50 +0200

    Created LobbyEnvironment class.

---
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/LobbyEnvironment.java
@@ -0,0 +1,9 @@
+package com.github.migi_1.Context.model;
+
+/**
+ * The LobbyEnvironment is a 3D environment that functions
+ * as the lobby screen.
+ */
+public class LobbyEnvironment extends Environment {
+    
+}

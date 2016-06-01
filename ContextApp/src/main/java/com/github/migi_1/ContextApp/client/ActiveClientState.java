2b0d1dd8ed8e4e00748957945a67f2128f87dff9
Luka Miljak <lmiljak@student.tudelft.nl>
2016-06-01 12:19:57 +0200

    Added Javadoc in ActiveClientState

--- ContextApp/src/main/java/com/github/migi_1/ContextApp/client/ActiveClientState.java
+++ ContextApp/src/main/java/com/github/migi_1/ContextApp/client/ActiveClientState.java
@@ -2,12 +2,25 @@ package com.github.migi_1.ContextApp.client;
 
 import com.jme3.network.Client;
 
+/**
+ * A client state for when the client is active.
+ */
 public class ActiveClientState extends ClientState {
 
+    /**
+     * Constructor for ActiveClientState.
+     * 
+     * @param client
+     *      The client that this state represents.
+     */
     public ActiveClientState(Client client) {
         super(client);
     }
     
+    /**
+     * Called when the state of the ClientWrapper changes to this state.
+     * The ActiveClientState starts the client.
+     */
     @Override
     public void onActivate() {
         getClient().start();

6c5bae24df5a0e85d97d99d10877af29db6b2ded
Damian Voorhout <Damianvoorhout@gmail.com>
2016-06-02 21:29:45 +0200

    Javadoc EnemyFactory

--- ContextDesktop/src/main/java/com/github/migi_1/Context/enemy/EnemyFactory.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/enemy/EnemyFactory.java
@@ -4,8 +4,6 @@ import com.github.migi_1.Context.model.entity.Carrier;
 import com.jme3.bounding.BoundingBox;
 import com.jme3.math.Vector3f;
 
-
-
 /**
  * This class is a factory that produces Enemy objects.
  * @author Damian
@@ -17,7 +15,14 @@ public class EnemyFactory {
     private float levelPieceLength;
     private float levelPieceWidth;
     private Carrier[] carriers;
-
+    
+    /**
+     * Constructor of the EnemyFactory.
+     * @param levelPieceBoundingBox the boundingbox of the levelPiece.
+     * @param levelPieceLength Length of the levelPiece.
+     * @param levelPieceWidth Width of the levelPiece.
+     * @param carriers the carriers on which the spawning will be based.
+     */
     public EnemyFactory(BoundingBox levelPieceBoundingBox, float levelPieceLength,
             float levelPieceWidth, Carrier[] carriers) {
         this.levelPieceBoundingBox = levelPieceBoundingBox;
@@ -26,6 +31,11 @@ public class EnemyFactory {
         this.carriers = carriers;
     }
     
+    /**
+     * Creates an enemy at the left side of the path, 2 levelPieces ahead of the commander.
+     * @param currentLevelPiece to calculate where to spawn the enemy
+     * @return the created enemy.
+     */
     public Enemy createEnemy1(double currentLevelPiece) {
         Enemy enemy =  new Enemy(new Vector3f(-(((int) currentLevelPiece + 2) * levelPieceLength),
                 carriers[0].getModel().getLocalTranslation().y,
@@ -35,6 +45,11 @@ public class EnemyFactory {
         return enemy;
     }
     
+    /**
+     * Creates an enemy at the left side of the path, 2 levelPieces ahead of the commander.
+     * @param currentLevelPiece to calculate where to spawn the enemy
+     * @return the created enemy.
+     */
     public Enemy createEnemy2(double currentLevelPiece) {
         Enemy enemy =  new Enemy(new Vector3f(-(((int) currentLevelPiece + 2) * levelPieceLength)
                 + levelPieceLength * 1 / 2, carriers[0].getModel().getLocalTranslation().y,
@@ -44,6 +59,11 @@ public class EnemyFactory {
         return enemy;
     }
     
+    /**
+     * Creates an enemy at the left side of the path, 2.5 levelPieces ahead of the commander.
+     * @param currentLevelPiece to calculate where to spawn the enemy
+     * @return the created enemy.
+     */
     public Enemy createEnemy3(double currentLevelPiece) {
         return new Enemy(new Vector3f(-(((int) currentLevelPiece + 2) * levelPieceLength)
                 + levelPieceLength * 1 / 2, carriers[0].getModel().getLocalTranslation().y,

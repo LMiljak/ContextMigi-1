6d03fee3d62ae362adb3c2f31969a86dd3acfb18
Damian Voorhout <Damianvoorhout@gmail.com>
2016-06-02 21:00:18 +0200

    javadoc enemyBehaviour

--- ContextDesktop/src/main/java/com/github/migi_1/Context/enemy/EnemyMoveBehaviour.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/enemy/EnemyMoveBehaviour.java
@@ -8,6 +8,11 @@ import com.github.migi_1.Context.model.entity.EnemySpot;
 import com.github.migi_1.Context.model.entity.MoveBehaviour;
 import com.jme3.math.Vector3f;
 
+/**
+ * Class responsible for the behaviour of enemies.
+ * @author Damian
+ *
+ */
 public class EnemyMoveBehaviour extends MoveBehaviour {
 
     private Vector3f moveVector;
@@ -17,7 +22,12 @@ public class EnemyMoveBehaviour extends MoveBehaviour {
     private EnemySpot targetSpot;
     private Enemy enemy;
     private boolean atSpot;
-
+    
+    /**
+     * Constructor of the EnemyMoveBehaviour.
+     * @param enemy The enemy to which the movebehaviour belongs.
+     * @param carriers for finding the spot where the enemy will walk to.
+     */
     public EnemyMoveBehaviour(Enemy enemy, Carrier[] carriers) {
         super();
         this.enemy = enemy;
@@ -27,7 +37,14 @@ public class EnemyMoveBehaviour extends MoveBehaviour {
         atSpot = false;
         targetSpot = createTargetSpot();
     }
-
+    
+    /**
+     * Finds the spot the enemy will walk to. 
+     * The spot is randomly chosen from all spots of the carriers which are still unoccupied.
+     * The spot becomes occupied as soon as the enemy starts walking towards it to prevent 2 enemies from
+     * walking to the same spot.
+     * @return The enemySpot, the spot where the enemy will walk towards.
+     */
     private EnemySpot createTargetSpot() {
         LinkedList<EnemySpot> spots = new LinkedList<EnemySpot>();
         for (Carrier carrier : carriers) {
@@ -50,7 +67,16 @@ public class EnemyMoveBehaviour extends MoveBehaviour {
     public Vector3f getMoveVector() {
         return moveVector;
     }
-
+    
+    /**
+     * Updates the moveVector.
+     * If the enemy has a moveVector (if not all spots are occupied already) and it is within range
+     * of this spot, the enemy will move towards it with a certain speed in the x and z direction.
+     * As soon as the distance on the x as is less than the speed it will move on that spot equal to
+     * that distance to prevent the enemy from overshooting that threshold. Overshooting results in 
+     * stuttering of the enemy when it has reached the target spot. It also sets the atSpot to true when 
+     * the enemy has reached the generated targetSpot at createTargetSpot.
+     */
     @Override
     public void updateMoveVector() {
         if (targetSpot != null) { 

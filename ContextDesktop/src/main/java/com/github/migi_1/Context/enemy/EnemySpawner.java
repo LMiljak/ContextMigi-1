4aaad97b1d7ea0c7928be812db8d124ed0fa3dd5
Damian Voorhout <Damianvoorhout@gmail.com>
2016-06-02 20:27:15 +0200

    javadoc enemy spawner

--- ContextDesktop/src/main/java/com/github/migi_1/Context/enemy/EnemySpawner.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/enemy/EnemySpawner.java
@@ -8,6 +8,13 @@ import com.github.migi_1.Context.model.entity.Commander;
 import com.jme3.bounding.BoundingBox;
 import com.jme3.math.Vector3f;
 
+/**
+ * Class which handles everything that has to do with spawning enemies.
+ * The enemyspawner has an enemyFactory which makes the actual enemies.
+ * The spawner is responsible for placing the enemies in the right location.
+ * @author TUDelft SID
+ *
+ */
 public class EnemySpawner {
     
     private EnemyFactory enemyFactory;
@@ -19,7 +26,12 @@ public class EnemySpawner {
     private double lastLevelPiece;
     private float levelPieceWidth;
     private Vector3f commanderLocation;
-
+    
+    /**
+     * Constructor of the EnemySpawner.
+     * @param commander to find the offset of the carriers and therefore the spots where to spawn the enemies.
+     * @param carriers the carriers which have spots where the enemies will walk to.
+     */
     public EnemySpawner(Commander commander, Carrier[] carriers) {
         enemies = new LinkedList<Enemy>();
         deleteList = new LinkedList<Enemy>();        
@@ -31,7 +43,15 @@ public class EnemySpawner {
         lastLevelPiece = -1;
         enemyFactory = new EnemyFactory(levelPieceBoundingBox, levelPieceLength, levelPieceWidth, carriers);
     }
-
+    
+    /**
+     * Determines how many enemies to spawn and where, returns a list of said enemies.
+     * It first calculates the levelPiece the carriage is currently on.
+     * It then spawns enemies randomly on both sides of the path, adds them to an empty list and returns it.
+     * It also adds enemies to the list of total enemies, which keeps track of all enemies in the game.
+     * The maximum amount of enemies in the game is 12.
+     * @return list of new enemies to add to the game.
+     */
     public LinkedList<Enemy> generateEnemies() {
         currentLevelPiece = -Math.floor(commanderLocation.x / levelPieceLength);
         LinkedList<Enemy> newEnemies = new LinkedList<Enemy>();
@@ -55,7 +75,13 @@ public class EnemySpawner {
         enemies.addAll(newEnemies);
         return newEnemies;        
     }
-
+    
+    /**
+     * Creates a list of enemies to delete.
+     * Enemies which need to be deleted are enemies which have no health and enemies which have fallen too far behind
+     * the commander.
+     * @return List of enemies to delete from the game.
+     */
     public LinkedList<Enemy> deleteEnemies() {
         for (Enemy enemy : enemies) {
             if (enemy.getHealth() <= 0) {

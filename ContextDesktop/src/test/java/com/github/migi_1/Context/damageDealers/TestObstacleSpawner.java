3d6ba13a50c05e02246bef7da7baad1170a135ea
Marcel Kuipers <mekuipers@tudelft.nl>
2016-06-10 19:00:37 +0200

    fixed testObstacleSpawner

--- ContextDesktop/src/test/java/com/github/migi_1/Context/damageDealers/TestObstacleSpawner.java
+++ ContextDesktop/src/test/java/com/github/migi_1/Context/damageDealers/TestObstacleSpawner.java
@@ -12,13 +12,15 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.github.migi_1.Context.main.Main;
+import com.github.migi_1.Context.model.MainEnvironment;
 import com.github.migi_1.Context.model.entity.Commander;
-import com.github.migi_1.Context.model.entity.behaviour.MoveBehaviour;
 import com.github.migi_1.Context.obstacle.AbstractObstacleFactory;
 import com.github.migi_1.Context.obstacle.ObstacleSpawner;
 import com.github.migi_1.Context.server.ServerWrapper;
 import com.github.migi_1.Context.utility.ProjectAssetManager;
 import com.jme3.asset.AssetManager;
+import com.jme3.bounding.BoundingBox;
+import com.jme3.bounding.BoundingVolume;
 import com.jme3.math.Vector3f;
 import com.jme3.network.Server;
 import com.jme3.scene.Spatial;
@@ -27,7 +29,7 @@ import com.jme3.scene.Spatial;
  * Test suite for the ObstacleSpawner class.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ProjectAssetManager.class, AssetManager.class, Main.class})
+@PrepareForTest({ProjectAssetManager.class, AssetManager.class, Main.class, BoundingVolume.class, BoundingBox.class})
 public class TestObstacleSpawner {
 
     private ProjectAssetManager pAssetManager;
@@ -38,18 +40,35 @@ public class TestObstacleSpawner {
     private Main main;
     private ServerWrapper serverWrapper;
     private Server server;
+    private MainEnvironment environment;
+    private Commander commander;
+    private BoundingBox leftBound;
+    private BoundingBox rightBound;
 
     /**
      * Setup for the tests.
+     * @throws Exception
+     * @throws SecurityException
+     * @throws NoSuchMethodException
      */
     @Before
-    public void setUp() {
+    public void setUp() throws NoSuchMethodException, SecurityException, Exception {
         pAssetManager = PowerMockito.mock(ProjectAssetManager.class);
         assetManager = Mockito.mock(AssetManager.class);
         model =  Mockito.mock(Spatial.class);
         main = Mockito.mock(Main.class);
         serverWrapper = Mockito.mock(ServerWrapper.class);
         server = Mockito.mock(Server.class);
+        environment = Mockito.mock(MainEnvironment.class);
+        commander = Mockito.mock(Commander.class);
+        leftBound =  new BoundingBox(
+                new Vector3f(0, 0, 0),
+                        Float.MAX_VALUE,
+                        100f, 1f);
+        rightBound =  new BoundingBox(
+                new Vector3f(0, 0, 0),
+                        Float.MAX_VALUE,
+                        100f, 1f);
         PowerMockito.mockStatic(ProjectAssetManager.class);
         PowerMockito.mockStatic(Main.class);
         BDDMockito.given(Main.getInstance()).willReturn(main);
@@ -58,8 +77,11 @@ public class TestObstacleSpawner {
         Mockito.when(assetManager.loadModel(Mockito.anyString())).thenReturn(model);
         Mockito.when(main.getServer()).thenReturn(serverWrapper);
         Mockito.when(serverWrapper.getServer()).thenReturn(server);
-
-        obstacleSpawner = new ObstacleSpawner(new Commander(new Vector3f(0, 0, 0), Mockito.mock(MoveBehaviour.class)));
+        Mockito.when(environment.getCommander()).thenReturn(commander);
+        Mockito.when(commander.getModel()).thenReturn(model);
+        Mockito.when(environment.getLeftBound()).thenReturn(leftBound);
+        Mockito.when(environment.getRightBound()).thenReturn(rightBound);
+        obstacleSpawner = new ObstacleSpawner(environment);
     }
 
     /**

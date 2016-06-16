bf6a74e10ed96a0d503e26f54c98644e2d1ad652
Marcel Kuipers <mekuipers@tudelft.nl>
2016-06-16 18:02:37 +0200

    initial method

--- ContextDesktop/src/main/java/com/github/migi_1/Context/model/LobbyEnvironment.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/model/LobbyEnvironment.java
@@ -2,6 +2,7 @@ package com.github.migi_1.Context.model;
 
 import com.github.migi_1.Context.main.LobbyHUDController;
 import com.github.migi_1.Context.model.entity.CarrierAssigner;
+import com.github.migi_1.Context.score.ScoreController;
 import com.github.migi_1.ContextMessages.PlatformPosition;
 import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
@@ -13,45 +14,51 @@ import com.jme3.renderer.ViewPort;
  * as the lobby screen.
  */
 public class LobbyEnvironment extends Environment {
-    
+
     private ViewPort viewPort;
-    
+
     private static final ColorRGBA BACKGROUNDCOLOR = ColorRGBA.Black;
-    
+
     private LobbyHUDController lobbyHUDController;
-    
+
     private CarrierAssigner carrierAssigner;
-    
+
+
+    private ScoreController scoreController;
+
     /**
      * Constructor for LobbyEnvironment.
-     * 
+     *
      * @param carrierAssigner
      * 		The CarrierAssigner (the same on the MainEnvironment should also get).
      */
     public LobbyEnvironment(CarrierAssigner carrierAssigner) {
     	this.carrierAssigner = carrierAssigner;
     }
-    
+
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
-        
+
         viewPort = app.getViewPort();
 
         lobbyHUDController = new LobbyHUDController(app);
+
+        scoreController = new ScoreController();
+        System.out.println(scoreController.getHighScore());
         viewPort.setBackgroundColor(BACKGROUNDCOLOR);
     }
-    
+
     @Override
     public void update(float tpf) {
     	super.update(tpf);
-    	
+
     	updatePlayerText();
     }
-    
+
     /**
      * Checks if the CarrierAssigner has assigned a carrier
-     * to a position and updates the position in the HUD if 
+     * to a position and updates the position in the HUD if
      * that's the case.
      */
     private void updatePlayerText() {
@@ -64,11 +71,11 @@ public class LobbyEnvironment extends Environment {
     		}
     	}
     }
-    
+
     @Override
     public void cleanup() {
         // viewPort.clearProcessors();
         super.cleanup();
     }
-    
+
 }
--- ContextDesktop/src/main/java/com/github/migi_1/Context/score/ScoreController.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/score/ScoreController.java
@@ -47,5 +47,9 @@ public class ScoreController {
         }
     }
 
+    public int getHighScore() {
+        return 42;
+    }
+
 
 }

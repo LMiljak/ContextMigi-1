7194fb64db3aea4d6e3d075eeae7ad1dee1f9d3f
Luka Miljak <lmiljak@student.tudelft.nl>
2016-06-16 11:27:55 +0200

    Fixed Carriers being in the wrong position.

--- ContextMessages/src/main/java/com/github/migi_1/ContextMessages/PlatformPosition.java
+++ ContextMessages/src/main/java/com/github/migi_1/ContextMessages/PlatformPosition.java
@@ -5,54 +5,51 @@ package com.github.migi_1.ContextMessages;
  */
 public enum PlatformPosition {
 
-	FRONTLEFT(-1, 1, "Location: front/left"),
-	FRONTRIGHT(-1, -1, "Location: front/right"),
-	BACKLEFT(1, 1, "Location: back/left"),
-	BACKRIGHT(1, -1, "Location: back/right");
+	FRONTLEFT(1, 1, "Location: front/left"),
+	FRONTRIGHT(1, -1, "Location: front/right"),
+	BACKLEFT(-1, 1, "Location: back/left"),
+	BACKRIGHT(-1, -1, "Location: back/right");
 
 	private int xFactor;
 	private int zFactor;
-        private String position;
+	private String position;
 
 	/**
 	 * Constructor for PlatformPosition.
 	 *
 	 * @param xFactor
-	 * 		-1 if it's in the front, 1 if it's in the back.
+	 *            -1 if it's in the front, 1 if it's in the back.
 	 * @param zFactor
-	 * 		1 if it's left, -1 if it's right.
-         * @param position
-         *              The string printed on an android device's screen
-         *              for the player to see in which position they are.
+	 *            1 if it's left, -1 if it's right.
+	 * @param position
+	 *            The string printed on an android device's screen for the
+	 *            player to see in which position they are.
 	 */
 	PlatformPosition(int xFactor, int zFactor, String position) {
 		this.xFactor = xFactor;
 		this.zFactor = zFactor;
-                this.position = position;
+		this.position = position;
 	}
 
 	/**
-	 * @return
-	 * 		-1 if this position in the front, 1 if it's in the back.
+	 * @return -1 if this position in the front, 1 if it's in the back.
 	 */
 	public int getxFactor() {
 		return xFactor;
 	}
 
 	/**
-	 * @return
-	 * 		1 if this position left, -1 if it's right.
+	 * @return 1 if this position left, -1 if it's right.
 	 */
 	public int getzFactor() {
 		return zFactor;
 	}
 
-        /**
-         * @return location
-         *              The location of the carrier.
-         */
-        public String getPosition() {
-                return position;
-        }
+	/**
+	 * @return location The location of the carrier.
+	 */
+	public String getPosition() {
+		return position;
+	}
 
 }

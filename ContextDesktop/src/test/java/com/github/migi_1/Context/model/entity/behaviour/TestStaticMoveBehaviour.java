861463b60ee5f595f51df7b1b26ee9cadf3dcd61
Nils <nhullegien@student.tudelft.nl>
2016-06-17 15:53:54 +0200

    Added tests for static move behaviour

--- ContextDesktop/src/test/java/com/github/migi_1/Context/model/entity/behaviour/TestStaticMoveBehaviour.java
+++ ContextDesktop/src/test/java/com/github/migi_1/Context/model/entity/behaviour/TestStaticMoveBehaviour.java
@@ -1,6 +1,8 @@
 package com.github.migi_1.Context.model.entity.behaviour;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  * Test class for the StaticMoveBehaviour class.
@@ -9,14 +11,24 @@ import org.junit.Before;
  */
 public class TestStaticMoveBehaviour extends TestMoveBehaviour {
 
+    private StaticMoveBehaviour behaviour;
+
     /**
      * Initialise the tested object.
      */
     @Override
     @Before
     public void setUp() {
-        setMoveBehaviour(new StaticMoveBehaviour());
+        behaviour = Mockito.spy(new StaticMoveBehaviour());
+        setMoveBehaviour(behaviour);
         setMoveVector(getMoveVector());
+    }
+
+    @Test
+    public void updateMoveVectorTest() {
+        behaviour.updateMoveVector();
+        Mockito.verify(behaviour).updateMoveVector();
+        Mockito.verifyNoMoreInteractions(behaviour);
     }
 
 }

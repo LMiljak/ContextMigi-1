6318938c2729ef83355f8598917123151e50aab3
Damian Voorhout <Damianvoorhout@gmail.com>
2016-06-17 16:31:49 +0200

    javadoc TestEnemySpot

--- ContextDesktop/src/test/java/com/github/migi_1/Context/model/TestEnemySpot.java
+++ ContextDesktop/src/test/java/com/github/migi_1/Context/model/TestEnemySpot.java
@@ -18,6 +18,11 @@ import com.github.migi_1.ContextMessages.PlatformPosition;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 
+/**
+ * Class for testing everything that has to do with an EnemySpot.
+ * @author Nils
+ *
+ */
 public class TestEnemySpot {
 
     private EnemySpot enemySpot;
@@ -25,6 +30,10 @@ public class TestEnemySpot {
     private Commander commander;
     private Spatial model;
 
+    /**
+     * Sets up everything needed for the tests. Happens before every test.   
+     * @exception Exception Any exception that is thrown.
+     */
     @Before
     public void setUp() throws Exception {
         carrier = Mockito.mock(Carrier.class);
@@ -37,40 +46,61 @@ public class TestEnemySpot {
         enemySpot = new EnemySpot(Vector3f.ZERO, carrier, commander, Direction.NORTH);
     }
 
+    /**
+     * Test the getter and setter of the occupied boolean.
+     */
     @Test
     public void getAndSetOccupiedTest() {
         assertFalse(enemySpot.isOccupied());
         enemySpot.setOccupied(true);
         assertTrue(enemySpot.isOccupied());
     }
-
+    
+    /**
+     * Tests the getter and setter of the carrier attribute.
+     */
     @Test
     public void getAndSetCarrierTest() {
         Carrier oldCarrier = enemySpot.getCarrier();
         enemySpot.setCarrier(Mockito.mock(Carrier.class));
         assertNotEquals(oldCarrier, enemySpot.getCarrier());
     }
-
+    
+    /**
+     * Tests the getLocation method.
+     */
     @Test
     public void getLocationTest() {
         assertEquals(Vector3f.ZERO, enemySpot.getLocation());
-    }
-
+    } 
+    
+    /**
+     * Tests the getOffset method.
+     */
     @Test
     public void getOffsetTest() {
         assertEquals(Vector3f.ZERO, enemySpot.getOffset());
     }
-
+    
+    /**
+     * Tests the getCarrierId method.
+     */
     @Test
     public void getCarrierIDTest() {
         assertEquals(PlatformPosition.BACKLEFT, enemySpot.getCarrierId());
     }
-
+    
+    /**
+     * Tests the getDirection method.
+     */
     @Test
     public void getDirectionTest() {
         assertEquals(Direction.NORTH, enemySpot.getDirection());
     }
-
+    
+    /**
+     * Tests the getter and setter methods for the enemy.
+     */
     @Test
     public void getAndSetEnemyTest() {
         Enemy oldEnemy =  enemySpot.getEnemy();

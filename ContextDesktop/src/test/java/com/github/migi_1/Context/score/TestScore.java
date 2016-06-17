3c0b12d988281ca1aed05c82edfddd3d3f1331ba
Damian Voorhout <Damianvoorhout@gmail.com>
2016-06-17 17:00:17 +0200

    javadoc TestScore

--- ContextDesktop/src/test/java/com/github/migi_1/Context/score/TestScore.java
+++ ContextDesktop/src/test/java/com/github/migi_1/Context/score/TestScore.java
@@ -8,13 +8,22 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+/**
+ * Tests everything that has to with the Score class.
+ * @author Nils
+ *
+ */
 public class TestScore {
 
     private Iterator<String> iterator;
     private Score score;
     private String scoreName = "scoreName";
     private int scoreAmount = 0;
-
+    
+    /**
+     * This method starts every time a new test case starts.
+     * @throws Exception exception that is thrown.
+     */
     @SuppressWarnings("unchecked")
     @Before
     public void setUp() {
@@ -23,21 +32,30 @@ public class TestScore {
 
         Mockito.when(iterator.next()).thenReturn("0");
     }
-
+    
+    /**
+     * Tests the getter and setter of the name attribute.
+     */
     @Test
     public void getAndSetNameTest() {
         assertEquals(scoreName, score.getName());
         score.setName("newname");
         assertEquals("newname", score.getName());
     }
-
+    
+    /**
+     * Tests the getter and setter of score.
+     */
     @Test
     public void getAndSetScoreTest() {
         assertEquals(0, score.getScore());
         score.setScore(42);
         assertEquals(42, score.getScore());
     }
-
+    
+    /**
+     * Tests the read method.
+     */
     @Test
     public void readTest() {
         Score readScore = Score.read(iterator);

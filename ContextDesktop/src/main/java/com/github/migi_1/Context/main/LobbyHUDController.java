a4172fc2263e2ccb6a4aec659e87744204fc74f7
Luka Miljak <lmiljak@student.tudelft.nl>
2016-06-17 12:32:37 +0200

    Added Javadoc to a private method.

--- ContextDesktop/src/main/java/com/github/migi_1/Context/main/LobbyHUDController.java
+++ ContextDesktop/src/main/java/com/github/migi_1/Context/main/LobbyHUDController.java
@@ -51,6 +51,9 @@ public class LobbyHUDController {
         addHighScore();
     }
     
+    /**
+     * Adds a text box for the HighScore.
+     */
     private void addHighScore() {
     	ScoreController scoreController = new ScoreController();
     	int highScore = scoreController.getHighScore();
